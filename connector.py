02575d003c7f974700a5837165bbcecd3360c57f
abdo <abdo.bougaa@gmail.com>
2022-03-27 10:06:20 +0000

    First commit

---
+++ connector.py
@@ -0,0 +1,55 @@
+from . import __version__
+import json
+import requests
+
+
+class Connector:
+
+    def __init__(self, url: str, login: str, password: str, db: str):
+        print("Odoo Connector Version: {}".format(__version__))
+        self._url = url
+        self._login = login
+        self._password = password
+        self._db = db
+        self._cookies: dict = {}
+        self._HEADER = {'Content-type': 'application/json'}
+
+    def auth(self):
+        req = requests.post(
+            self._url + '/auth/',
+            data=json.dumps({
+                'params': {
+                    'login': self._login,
+                    'password': self._password,
+                    'db': self._db
+                }
+            }),
+            headers=self._HEADER
+        )
+        if not json.loads(req.text).get('result', False):
+            return False
+        return dict(req.cookies)
+
+    def connect(self):
+        if not self._cookies.get('session_id', False):
+            self._cookies = self.auth()
+            if not self._cookies:
+                raise Exception('Error cnx')
+        return self._cookies
+
+    def post_value(self, url, dt):
+        cnx = requests.post(
+            url=self._url + url,
+            headers=self._HEADER,
+            data=json.dumps({'params': dt}),
+            cookies=self.connect()
+        ).text
+        if json.loads(cnx).get('error', {'code': False})['code'] == 100:
+            self._cookies = {}
+            cnx = requests.post(
+                url=self._url + url,
+                headers=self._HEADER,
+                data=json.dumps({'params': dt}),
+                cookies=self.connect()
+            ).text
+        return cnx
