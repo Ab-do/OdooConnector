5c88cf7af54b848df4c0434ecd22726c9b887cc8
abdo <abdo.bougaa@gmail.com>
2022-03-29 10:23:53 +0000

    add deco cookies

--- connector.py
+++ connector.py
@@ -3,6 +3,16 @@ import json
 import requests
 
 
+def cookies(func):
+    def inner(*args, **kwargs):
+        r = func(*args, **kwargs)
+        if json.loads(r).get('error', {'code': False})['code'] == 100:
+            args[0]._cookies = {}
+            return func(*args, **kwargs)
+        return r
+    return inner
+
+
 class Connector:
 
     def __init__(self, url: str, login: str, password: str, db: str):
@@ -11,7 +21,7 @@ class Connector:
         self._login = login
         self._password = password
         self._db = db
-        self._cookies: dict = {}
+        self._cookies: dict = {'session_id': '28befa61980463f9d51acb9e22c5ab6c87fbeda0d'}
         self._HEADER = {'Content-type': 'application/json'}
         print("Model Odoo Version: {}".format(self.get('/odoo-connector-v')))
 
@@ -38,6 +48,7 @@ class Connector:
                 raise Exception('Error cnx')
         return self._cookies
 
+    @cookies
     def post(self, url, dt):
         cnx = requests.post(
             url=self._url + url,
@@ -45,18 +56,10 @@ class Connector:
             data=json.dumps({'params': dt}),
             cookies=self._connect()
         ).text
-        if json.loads(cnx).get('error', {'code': False})['code'] == 100:
-            self._cookies = {}
-            cnx = requests.post(
-                url=self._url + url,
-                headers=self._HEADER,
-                data=json.dumps({'params': dt}),
-                cookies=self._connect()
-            ).text
         return cnx
 
     def get(self, url):
-        cnx = requests.get(self._url+url)
+        cnx = requests.get(self._url + url)
         if cnx.status_code == 200:
             return cnx.text
         else:
